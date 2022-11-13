3cbd7c9f4d47e058c8089a309895dd163b72d961
RizqiRHuda <rizqirhuda45@gmail.com>
2022-11-13 12:23:00 +0700

    up files

--- pertemuan10/praktik_php/array_2.php
+++ pertemuan10/praktik_php/array_2.php
@@ -4,7 +4,9 @@
     <body>
         <h2> Indexed Array </h2>
         <?php
-            $buah = array("Mangga", "Apel", "Jeruk");
+            $buah[0] = "Mangga";
+            $buah[1] = "Apel";
+            $buah[2] = "Jeruk"; 
 
             echo "Buah " . $buah[0]. " dan ". $buah[1] . " rasanya manis sekali";
         ?>
