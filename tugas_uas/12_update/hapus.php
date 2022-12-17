5a87ca32a75a6787ee6d3945ecb1e84195166a82
RizqiRHuda <rizqirhuda45@gmail.com>
2022-12-17 20:25:57 +0700

    upload

--- tugas_uas/12_update/hapus.php
+++ tugas_uas/12_update/hapus.php
@@ -1,26 +1,22 @@
 <?php
 require 'functions.php';
 
-//menggunakan method get untuk mengambil id yang telah terseleksi oleh user dan dimasukkan
-// ke dalam variable baru yaitu id
-$id=$_GET["id"];
+$id = $_GET["id"];
 
-if( hapus($id)>0){
+
+if (hapus(($id) > 0)) {
     echo "
-    <script>
-        alert('data berhasil dihapus');
-        document.location.href='index.php';
-    </script>
+        <script>
+            alert('data berhasil dihapus');
+            document.location.href='index.php';
+        </script>
     ";
-}
-else{
+} else {
     echo "
     <script>
-        alert('data gagal dihapus');
+        alert('data gagala dihapus');
         document.location.href='index.php';
-    </script>
-    "; 
+    </script>";
     echo "<br>";
-    echo mysqli_error($conn);    
+    echo mysqli_error($conn);
 }
-?>
--- tugas_uas/12_update/index.php
+++ tugas_uas/12_update/index.php
@@ -1,60 +1,70 @@
 <?php
 require 'functions.php';
-$mahasiswa=query("  SELECT * FROM mahasiswa");
+$mahasiswa = query("SELECT * FROM mahasiswa");
+// var_dump($result);
+// die();
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Mahasiswa</title>
+    <title>Daftar Mahasiswa</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: 'Ubuntu', sans-serif;
+        }
+
+        th {
+            background-color: wheat;
+        }
+    </style>
 </head>
+
 <body>
-<h1>Daftar Mahasiswa</h1>
+    <h1>Daftar Mahasiswa</h1>
+    <a href="tambah_data.php">Tambah Data Mahasiswa</a>
 
-<a href="tambah_data.php">Tambah Data Mahasiswa</a>
+    <br><br>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+            <th>Gambar</th>
+            <th>Aksi</th>
+        </tr>
 
-        <table border="1" cellpading="10" cellspacing="0">
+        <?php $i = 1; ?>
+
+        <!-- Contoh data Static -->
+        <?php foreach ($mahasiswa as $row) : ?>
             <tr>
-                <th>No.</th>
-                <th>Nama</th>
-                <th>NIM</th>
-                <th>Email</th>
-                <th>Jurusan</th>
-                <th>Gambar</th>
-                <th>Aksi</th>
-            </tr>
-            <?php $i=1 ?>
-           
-            <?php foreach($mahasiswa as $row):?>
-                <tr>
+                <td><?= $i; ?></td>
+                <td><?= $row["Nama"]; ?></td>
+                <td><?= $row["Nim"]; ?></td>
+                <td><?= $row["Email"]; ?></td>
+                <td><?= $row["Jurusan"]; ?></td>
+                <td><img src="image/<?php echo $row["Gambar"]; ?>" alt="" height="100" width="100" srcset=""></td>
                 <td>
-                    <?= $i; ?>
-                </td>
-                <td>
-                    <?= $row["Nama"]; ?>
-                </td>
-                <td>
-                    <?= $row["Nim"]; ?>
-                </td>
-                <td>
-                    <?= $row["Email"]; ?>
-                </td>
-                <td>
-                    <?= $row["Jurusan"]; ?>
-                </td>
-                <td>
-                    <img src="image/<?php echo $row["Gambar"]; ?>" alt="" height="100">
-                </td>
-                <td>
-                    <a href="edit.php?id=<?php echo $row["id"];?>">Edit</a>
-                    <a href="hapus.php?id=<?php echo $row["id"];?>"onclick="return confirm('Apakah data ini akan dihapus?')">Hapus</a>
+                    <a href="edit.php?id=<?php echo $row["id"]; ?>">Edit</a>
+                    <a href="hapus.php?id=<?php echo $row["id"]; ?>" onclick="return confirm('Apa kamu yakin ingin menghapus data ini?')">Hapus</a>
                 </td>
             </tr>
             <?php $i++ ?>
-            <?php endforeach;?>
-        </table>
+        <?php endforeach; ?>
+    </table>
 </body>
+
 </html>
